b246633dd872a200c7f31d747b3cc12fc54bcc5a
yvonne <yvonnewong1107@gmail.com>
2024-01-09 14:36:06 +0000

    add descriptions for projects data

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './style.css';
+import Projects from './projects.json';
 
 function Cards(props) {
-    const {name, github, image} = props;
+    const {name, image, github, description} = props;
 
     return (
         <div className="card" style={{width: "18rem"}}>
             <img src={image} className="card-img-top" alt={`${name}`}></img>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">{description}</p>
                 <a href={github} className="btn btn-primary" target='_blank'>Go somewhere</a>
             </div>
         </div>
