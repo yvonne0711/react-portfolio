68c8543d95923df8756ccc56062db865bf5820b3
yvonne <yvonnewong1107@gmail.com>
2024-11-30 00:23:39 +0000

    footer style

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./style.css";
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-      <p>
-        Footer
-      </p>
-    </div>
+    <footer>
+      <h1 className="footer-title">Let's keep in touch</h1>
+      <div className="footer-btns">
+        <a href="mailto: yvonnewong1107@gmail.com" className="btn btn-contact" target="_blank"><i className="fa fa-envelope"></i></a>
+        <a href="https://github.com/yvonne0711" className="btn btn-contact" target="_blank"><i className="fa fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/yvonnewong1107/" className="btn btn-contact" target="_blank"><i className="fa fa-linkedin-square"></i></a>
+      </div>
+    </footer>
   );
 }
 
