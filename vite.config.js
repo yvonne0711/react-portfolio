e67e0aa6828109393098e71cbaf97663f4180099
yvonne <yvonnewong1107@gmail.com>
2024-12-03 14:02:03 +0000

    package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-portfolio/"
-})
+  base: process.env.NODE_ENV === 'development' ? '/' : '/react-portfolio/',  // Conditionally set base
+});
